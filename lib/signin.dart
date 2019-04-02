c7cd69a05765f5580ff58f966730c90bf46e8a5c
mj <onyangomaureen95@gmail.com>
2019-04-02 23:19:58 +0300

    create sign in class

---
+++ lib/signin.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Signin extends StatelessWidget{
+  Widget build(context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+              ))
+ ); }
+}
