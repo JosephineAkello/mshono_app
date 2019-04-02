8927b6e7a0640a73ceff645b287c62c87f78805a
mj <onyangomaureen95@gmail.com>
2019-04-02 23:41:57 +0300

    create username widgets,passwords and submit button

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
-class Signin extends StatelessWidget{
-  Widget build(context){
+class Signin extends StatelessWidget {
+  Widget build(context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-              ))
- ); }
+        home: Scaffold(
+            body: Container(
+                child: Form(
+                    child: SingleChildScrollView(
+      child: Column(children: [
+        SizedBox(
+          height: 50.0,
+        ),
+        username(),
+        password(),
+      ]),
+    )))));
+  }
+
+  Widget username() {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+        labelText: 'username',
+        hintText: 'josephine',
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
+      ),
+    );
+  }
+  }
+
+  Widget password() {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+        labelText: 'password',
+        hintText: '123456',
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
+      ),
+    );
+  }
+  
+
+  Widget submit() {
+    return RaisedButton(
+        child: Text('Register'),
+      color: Colors.pink,
+      padding: EdgeInsets.all(20.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      onPressed: () {},
+     );
+  
 }
