737b36955c9e35177ff5c8a22df4a0ca21737db3
mj <onyangomaureen95@gmail.com>
2019-04-03 15:03:35 +0300

    create homepage

---
+++ lib/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  Widget build(conetxt){
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+} 
