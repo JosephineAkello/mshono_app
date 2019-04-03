13cf2953842a5888b9ee5286580bdaef2b6ecf8a
mj <onyangomaureen95@gmail.com>
2019-04-03 12:44:19 +0300

    initialize form key and add email widget

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,35 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Signin extends StatefulWidget {
-createState(){
-  return SigninState();
-}
+  createState() {
+    return SigninState();
+  }
 }
 
-class SigninState extends State<Signin>{
+class SigninState extends State<Signin> {
+  final formKey = GlobalKey<FormState>();
 
   Widget build(context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.pink),
+        theme: ThemeData(primaryColor: Colors.pink),
         home: Scaffold(
-            body: Container(
-                child: Form(
-                    child: SingleChildScrollView(
-      child: Column(children: [
-        SizedBox(
-          height: 50.0,
-        ),
-        username(),
-        SizedBox(
-            height: 20.0,
-          ),
-        password(),
-        SizedBox(
-            height: 20.0,
-          ),
-          submit(),
-      ]),
-    )))));
+        body: Container(
+          child: Form(
+            key: formKey,
+          child: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(
+              height: 50.0,
+            ),
+            username(),
+            SizedBox(
+              height: 20.0,
+            ),
+            password(),
+            SizedBox(
+              height: 20.0,
+            ),
+            submit(),
+          ]),
+        )))));
   }
 
   Widget username() {
@@ -42,11 +44,20 @@ class SigninState extends State<Signin>{
       ),
     );
   }
+
+  Widget email() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'emailAddress',
+        hintText: 'jos@gmail.com',
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
+      ),
+    );
   }
 
   Widget password() {
     return TextFormField(
-      keyboardType: TextInputType.text,
       decoration: InputDecoration(
         labelText: 'password',
         hintText: '123456',
@@ -54,15 +65,14 @@ class SigninState extends State<Signin>{
       ),
     );
   }
-  
 
   Widget submit() {
     return RaisedButton(
-        child: Text('Login'),
+      child: Text('Login'),
       color: Colors.pink,
       padding: EdgeInsets.all(20.0),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       onPressed: () {},
-     );
-  
+    );
+  }
 }
