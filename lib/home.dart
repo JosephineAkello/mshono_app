fdc3130b21eaf2c73c9532cfddf6579299208c36
mj <onyangomaureen95@gmail.com>
2019-04-04 09:49:34 +0300

    add a drawer with a drawer header

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,28 @@ class HomePage extends StatelessWidget{
   Widget build(conetxt){
     return MaterialApp(
       home: Scaffold(
-        body: Container(),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                child: CircleAvatar(
+                  child: Text('E-tailor',
+                  style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.pink)),
+                  backgroundColor: Colors.white,
+                radius: 100.0,
+               ),
+               decoration: BoxDecoration(
+                 color: Colors.pink
+               ),
+                ),
+              
+            ],
+          ),
+        ),
+        body: Text('home page'),
       ),
     );
   }
