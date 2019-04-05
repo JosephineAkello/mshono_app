8c15a4c1bb879a9cb5293270c9ae4bc90c862970
mj <onyangomaureen95@gmail.com>
2019-04-05 09:49:16 +0300

    create designs class

---
+++ lib/fragments/designs.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Designs extends StatelessWidget{
+  Widget build(context){
+    return MaterialApp(
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
