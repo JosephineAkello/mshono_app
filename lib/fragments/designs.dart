aa4782bfb634bad486852a9b3082d8cdd254da44
mj <onyangomaureen95@gmail.com>
2019-04-07 11:32:30 +0300

    create a list view

--- lib/fragments/designs.dart
+++ lib/fragments/designs.dart
@@ -4,7 +4,14 @@ class Designs extends StatelessWidget{
   Widget build(context){
     return MaterialApp(
       home: Scaffold(
-        
+        body: ListView(
+          children: <Widget>[
+            Card(
+              child: ListTile(title: Text('palazo pants'),
+              leading: Icon(Icons.account_balance_wallet),)
+            )
+          ],
+        ),
       ),
     );
   }
