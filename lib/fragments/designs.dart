547c880b764fe849c285d0f60ca59b075e81d598
mj <onyangomaureen95@gmail.com>
2019-04-07 16:28:32 +0300

    place images in a grid view

--- lib/fragments/designs.dart
+++ lib/fragments/designs.dart
@@ -4,65 +4,53 @@ class Designs extends StatelessWidget{
   Widget build(context){
     return MaterialApp(
       home: Scaffold(
-        body: ListView(
+        body: GridView.count(
+           crossAxisCount: 2,
           children: <Widget>[
-            Card(
-              child: ListTile(
-               title: Image.asset(
+            GridTile(
+              child: Card(
+               
+               child: Image.asset(
                 'assets/pallazo.jpg',
               ),
-               subtitle: Text('palazo pants'),
-             )),
-             Card(
-              child: ListTile(
-               title: Image.asset(
+              ) ),
+              GridTile(
+              child: Card(
+               child: Image.asset(
                 'assets/dress.jpg',
-              ),
-               subtitle: Text('Dress'),
-             )),
-             Card(
-              child: ListTile(
-               title: Image.asset(
+              )) ),
+              GridTile(
+              child: Card(
+               child: Image.asset(
                 'assets/peplum.jpg',
-              ),
-               subtitle: Text('Coat'),
-             )),
-             Card(
-              child: ListTile(
-               title: Image.asset(
+              )) ),
+              GridTile(
+              child: Card(
+               child: Image.asset(
                 'assets/shirts.jpg',
-              ),
-               subtitle: Text('Shirt'),
-             )),
-             Card(
-              child: ListTile(
-               title: Image.asset(
+              )) ),
+              GridTile(
+              child: Card(
+               child: Image.asset(
                 'assets/shorts.jpg',
-              ),
-               subtitle: Text('Short'),
-             )),
-             Card(
-              child: ListTile(
-               title: Image.asset(
+              )) ),
+              GridTile(
+              child: Card(
+               child: Image.asset(
                 'assets/tishat.jpg',
-              ),
-               subtitle: Text('T-shirt'),
-             )),
-             Card(
-              child: ListTile(
-               title: Image.asset(
+              )) ),
+              GridTile(
+              child: Card(
+               child: Image.asset(
                 'assets/trouser.jpg',
-              ),
-               subtitle: Text('Trouser'),
-             )),
-             Card(
-              child: ListTile(
-               title: Image.asset(
+              )) ),
+             GridTile(
+              child: Card(
+               child: Image.asset(
                 'assets/skirt.jpg',
               ),
-               subtitle: Text('Skirt'),
-             )),
-          ],
+              ),
+           ) ],
         ),
       ),
     );
