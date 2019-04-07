9f1a3b1db0ae9047a279ffdec7544f04d6b48fe4
mj <onyangomaureen95@gmail.com>
2019-04-07 15:29:50 +0300

    add a list of designs

--- lib/fragments/designs.dart
+++ lib/fragments/designs.dart
@@ -7,9 +7,61 @@ class Designs extends StatelessWidget{
         body: ListView(
           children: <Widget>[
             Card(
-              child: ListTile(title: Text('palazo pants'),
-              leading: Icon(Icons.account_balance_wallet),)
-            )
+              child: ListTile(
+               title: Image.asset(
+                'assets/pallazo.jpg',
+              ),
+               subtitle: Text('palazo pants'),
+             )),
+             Card(
+              child: ListTile(
+               title: Image.asset(
+                'assets/dress.jpg',
+              ),
+               subtitle: Text('Dress'),
+             )),
+             Card(
+              child: ListTile(
+               title: Image.asset(
+                'assets/peplum.jpg',
+              ),
+               subtitle: Text('Coat'),
+             )),
+             Card(
+              child: ListTile(
+               title: Image.asset(
+                'assets/shirts.jpg',
+              ),
+               subtitle: Text('Shirt'),
+             )),
+             Card(
+              child: ListTile(
+               title: Image.asset(
+                'assets/shorts.jpg',
+              ),
+               subtitle: Text('Short'),
+             )),
+             Card(
+              child: ListTile(
+               title: Image.asset(
+                'assets/tishat.jpg',
+              ),
+               subtitle: Text('T-shirt'),
+             )),
+             Card(
+              child: ListTile(
+               title: Image.asset(
+                'assets/trouser.jpg',
+              ),
+               subtitle: Text('Trouser'),
+             )),
+             Card(
+              child: ListTile(
+               title: Image.asset(
+                'assets/skirt.jpg',
+              ),
+               subtitle: Text('Skirt'),
+             )),
           ],
         ),
       ),
