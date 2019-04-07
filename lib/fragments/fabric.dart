29ac35f1da49566c8ee00d046945cfcbcc1cfba7
mj <onyangomaureen95@gmail.com>
2019-04-07 18:02:20 +0300

    add a grid view of fabric

--- lib/fragments/fabric.dart
+++ lib/fragments/fabric.dart
@@ -4,7 +4,98 @@ class Fabric extends StatelessWidget{
   Widget build(context){
     return MaterialApp(
       home: Scaffold(
-        
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
+            Card(
+                child: GridTile(
+              child: Image.asset(
+                'assets/cotton.jpg',
+              ),
+              footer: Text(
+                'Cotton fabric',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )),
+            Card(
+                child: GridTile(
+              child: Image.asset(
+                'assets/denim.jpg',
+              ),
+              footer: Text(
+                'Denim fabric',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )),
+            Card(
+                child: GridTile(
+              child: Image.asset(
+                'assets/kitenge.jpg',
+              ),
+              footer: Text(
+                'Kitenge fabric',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )),
+            Card(
+                child: GridTile(
+              child: Image.asset(
+                'assets/lace.jpg',
+              ),
+              footer: Text(
+                'Lace fabric',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )),
+            Card(
+                child: GridTile(
+              child: Image.asset(
+                'assets/silks.jpg',
+              ),
+              footer: Text(
+                'Silk fabric',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )),
+            Card(
+                child: GridTile(
+              child: Image.asset(
+                'assets/velvet.jpg',
+              ),
+              footer: Text(
+                'Velvet fabric',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )),
+            Card(
+                child: GridTile(
+              child: Image.asset(
+                'assets/vintage.jpg',
+              ),
+              footer: Text(
+                'Vintage fabric',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )),
+            Card(
+                child: GridTile(
+              child: Image.asset(
+                'assets/leathers.jpg',
+              ),
+              footer: Text(
+                'Leather fabric',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )), ]),
+            
       ),
     );
   }
