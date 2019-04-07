77aeb13b3bea22d29f77203e82ddddc1e89a058a
mj <onyangomaureen95@gmail.com>
2019-04-07 17:41:33 +0300

    format document

--- lib/fragments/designs.dart
+++ lib/fragments/designs.dart
@@ -8,57 +8,98 @@ class Designs extends StatelessWidget {
           crossAxisCount: 2,
           children: <Widget>[
             Card(
+                child: GridTile(
+              child: Image.asset(
+                'assets/pallazo.jpg',
+              ),
+              footer: Text(
+                'Pallazo Pants',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )),
+            Card(
               child: GridTile(
                 child: Image.asset(
-                  'assets/pallazo.jpg',
+                  'assets/dress.jpg',
                 ),
-                footer: Text('Pallazo Pants', textAlign: TextAlign.justify, style: TextStyle(fontWeight: FontWeight.bold),),
-               )),
-        
+                footer: Text(
+                  'Dress',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Card(
+                child: GridTile(
+              child: Image.asset(
+                'assets/peplum.jpg',
+              ),
+              footer: Text(
+                'Coats',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )),
             Card(
               child: GridTile(
-                    child: Image.asset(
-              'assets/dress.jpg',
-            ),
-            footer: Text('Dress', textAlign: TextAlign.justify, style: TextStyle(fontWeight: FontWeight.bold),),),
+                child: Image.asset(
+                  'assets/shirts.jpg',
+                ),
+                footer: Text(
+                  'Shirts',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
             Card(
-                child: GridTile(
-                    child: Image.asset(
-              'assets/peplum.jpg',
+              child: GridTile(
+                child: Image.asset(
+                  'assets/shorts.jpg',
+                ),
+                footer: Text(
+                  'Shorts',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
-            footer: Text('Coats', textAlign: TextAlign.justify, style: TextStyle(fontWeight: FontWeight.bold),),)),
             Card(
-                child: GridTile(
-                    child: Image.asset(
-              'assets/shirts.jpg',
+              child: GridTile(
+                child: Image.asset(
+                  'assets/tishat.jpg',
+                ),
+                footer: Text(
+                  'T-shirts',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
-            footer: Text('Shirts', textAlign: TextAlign.justify, style: TextStyle(fontWeight: FontWeight.bold),),),),
             Card(
-                child: GridTile(
-                    child: Image.asset(
-              'assets/shorts.jpg',
+              child: GridTile(
+                child: Image.asset(
+                  'assets/trouser.jpg',
+                ),
+                footer: Text(
+                  'Trouser',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
-            footer: Text('Shorts', textAlign: TextAlign.justify, style: TextStyle(fontWeight: FontWeight.bold),),),),
             Card(
-                child: GridTile(
-                    child: Image.asset(
-              'assets/tishat.jpg',
-            ),
-            footer: Text('T-shirts', textAlign: TextAlign.justify, style: TextStyle(fontWeight: FontWeight.bold),),),),
-            Card(
-                child: GridTile(
-                    child: Image.asset(
-              'assets/trouser.jpg',
-            ),
-            footer: Text('Trouser', textAlign: TextAlign.justify, style: TextStyle(fontWeight: FontWeight.bold),),),),
-            Card(
-                child: GridTile(
+              child: GridTile(
                 child: Image.asset(
                   'assets/skirt.jpg',
                 ),
-                footer: Text('Skirt', textAlign: TextAlign.justify, style: TextStyle(fontWeight: FontWeight.bold),),),
-              
+                footer: Text(
+                  'Skirt',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
             )
           ],
         ),
