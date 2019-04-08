4c16de3a7d06560cdae7f8c78075ff71762a7b4d
mj <onyangomaureen95@gmail.com>
2019-04-08 12:30:44 +0300

    create designlist class

---
+++ lib/fragments/designlists.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class buildDesignList extends StatefulWidget {
+  createState() {
+    return buildDesignListState();
+  }
+}
+
+class buildDesignListState extends State<buildDesignList> {
+  Widget build(context) {
+    return InkWell(
+      onTap: () {},
+    );
+  }
+}
