11c301c7faee36ded63a6de80edce15b09a0dffb
mj <onyangomaureen95@gmail.com>
2019-04-08 12:39:49 +0300

    create design details

---
+++ lib/fragments/designdetails.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DesignDetails extends StatefulWidget{
+  createState(){
+    return DesignDetailsState();
+  }
+}
+
+class DesignDetailsState extends State<DesignDetails>{
+  Widget build(context){
+    return MaterialApp(
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
