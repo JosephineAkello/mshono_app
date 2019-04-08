1e818392bea0b4e7f1ea53e4e31fbf444cc2d874
mj <onyangomaureen95@gmail.com>
2019-04-08 11:49:39 +0300

    set debug mode to false

--- lib/fragments/designs.dart
+++ lib/fragments/designs.dart
@@ -14,17 +14,15 @@ class Designs extends StatelessWidget {
                 'assets/pallazo.jpg',
               ),
               footer: Container(
-                  color: Colors.white,
-                  child: ListTile(
-                      leading: Text(
-                        'Pallazo Pants',
-                        textAlign: TextAlign.justify,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      trailing: Icon(
-                        Icons.favorite_border,
-                        color: Colors.red,
-                      ))),
+                color: Colors.white,
+                child:  ListTile(
+                  leading: Text('Pallazo Pants',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),),
+                trailing: Icon(Icons.favorite_border, color: Colors.red,)
+                )
+                
+              ),
             )),
             Card(
               child: GridTile(
@@ -32,17 +30,15 @@ class Designs extends StatelessWidget {
                   'assets/dress.jpg',
                 ),
                 footer: Container(
-                    color: Colors.white,
-                    child: ListTile(
-                        leading: Text(
-                          'Dress',
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Icon(
-                          Icons.favorite,
-                          color: Colors.red,
-                        ))),
+                color: Colors.white,
+                child:  ListTile(
+                  leading: Text('Dress',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),),
+                trailing: Icon(Icons.favorite, color: Colors.red,)
+                )
+                
+              ),
               ),
             ),
             Card(
@@ -50,18 +46,16 @@ class Designs extends StatelessWidget {
               child: Image.asset(
                 'assets/peplum.jpg',
               ),
-              footer: Container(
-                  color: Colors.white,
-                  child: ListTile(
-                      leading: Text(
-                        'Coats',
-                        textAlign: TextAlign.justify,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      trailing: Icon(
-                        Icons.favorite_border,
-                        color: Colors.red,
-                      ))),
+             footer: Container(
+                color: Colors.white,
+                child:  ListTile(
+                  leading: Text('Coats',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),),
+                trailing: Icon(Icons.favorite_border, color: Colors.red,)
+                )
+                
+              ),
             )),
             Card(
               child: GridTile(
@@ -69,17 +63,15 @@ class Designs extends StatelessWidget {
                   'assets/shirts.jpg',
                 ),
                 footer: Container(
-                    color: Colors.white,
-                    child: ListTile(
-                        leading: Text(
-                          'Shirts',
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Icon(
-                          Icons.favorite_border,
-                          color: Colors.red,
-                        ))),
+                color: Colors.white,
+                child:  ListTile(
+                  leading: Text('Shirts',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),),
+                trailing: Icon(Icons.favorite_border, color: Colors.red,)
+                )
+                
+              ),
               ),
             ),
             Card(
@@ -88,17 +80,15 @@ class Designs extends StatelessWidget {
                   'assets/shorts.jpg',
                 ),
                 footer: Container(
-                    color: Colors.white,
-                    child: ListTile(
-                        leading: Text(
-                          'Shorts',
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Icon(
-                          Icons.favorite,
-                          color: Colors.red,
-                        ))),
+                color: Colors.white,
+                child:  ListTile(
+                  leading: Text('Shorts',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),),
+                trailing: Icon(Icons.favorite_border, color: Colors.red,)
+                )
+                
+              ),
               ),
             ),
             Card(
@@ -107,17 +97,15 @@ class Designs extends StatelessWidget {
                   'assets/tishat.jpg',
                 ),
                 footer: Container(
-                    color: Colors.white,
-                    child: ListTile(
-                        leading: Text(
-                          'T-shirts',
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Icon(
-                          Icons.favorite_border,
-                          color: Colors.red,
-                        ))),
+                color: Colors.white,
+                child:  ListTile(
+                  leading: Text('T-shirts',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),),
+                trailing: Icon(Icons.favorite_border, color: Colors.red,)
+                )
+                
+              ),
               ),
             ),
             Card(
@@ -126,17 +114,15 @@ class Designs extends StatelessWidget {
                   'assets/trouser.jpg',
                 ),
                 footer: Container(
-                    color: Colors.white,
-                    child: ListTile(
-                        leading: Text(
-                          'Trouser',
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Icon(
-                          Icons.favorite_border,
-                          color: Colors.red,
-                        ))),
+                color: Colors.white,
+                child:  ListTile(
+                  leading: Text('Trouser',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),),
+                trailing: Icon(Icons.favorite_border, color: Colors.red,)
+                )
+                
+              ),
               ),
             ),
             Card(
@@ -145,17 +131,15 @@ class Designs extends StatelessWidget {
                   'assets/skirt.jpg',
                 ),
                 footer: Container(
-                    color: Colors.white,
-                    child: ListTile(
-                        leading: Text(
-                          'Skirt',
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Icon(
-                          Icons.favorite_border,
-                          color: Colors.red,
-                        ))),
+                color: Colors.white,
+                child:  ListTile(
+                  leading: Text('Skirt',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontWeight: FontWeight.bold),),
+                trailing: Icon(Icons.favorite_border, color: Colors.red,)
+                )
+                
+              ),
               ),
             )
           ],
