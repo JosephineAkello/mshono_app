9cc856b6bbdb00552334cb6d99bdd4b0174822bc
mj <onyangomaureen95@gmail.com>
2019-04-10 00:16:35 +0300

    place image and name details

--- lib/fragments/buildfabricdetails.dart
+++ lib/fragments/buildfabricdetails.dart
@@ -19,8 +19,41 @@ class FabricDetails extends StatefulWidget{
 
 class FabricDetailsState extends State<FabricDetails>{
   Widget build(context){
-    return Container(
-
-    );
+  return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.purple[100],
+              shape:  CircleBorder(),
+              child: Icon(
+              Icons.shopping_cart,
+              color: Colors.purple,
+             ),
+             onPressed: (){}, ),
+            body: ListView(children: [
+              Container(
+                height: 300,
+                child: GridTile(
+                  child: Container(
+                      child: Image.asset(
+                    widget.fabricdetailsimage,
+                  )),
+                  footer: Container(
+                      color: Colors.white,
+                      child: ListTile(
+                        leading: Text(
+                          widget.fabricdetailsname,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20.0),
+                        ),
+                        trailing: Text('Price:  \$${widget.fabricdetailsprice}',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.purpleAccent)),
+                      )),
+                ),
+              ),]
+    ) ) );
   }
 }
