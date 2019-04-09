21a68645ea874b37f752fe66d9b5628d105a4ad5
mj <onyangomaureen95@gmail.com>
2019-04-09 12:25:31 +0300

    place design details like price

--- lib/fragments/designdetails.dart
+++ lib/fragments/designdetails.dart
@@ -1,36 +1,52 @@
 import 'package:flutter/material.dart';
 
-class DesignDetails extends StatefulWidget{
-final designDetailsName;
-final designDetailsImage;
-final designDetailsPrice;
-final designDetailsDescription;
+class DesignDetails extends StatefulWidget {
+  final designDetailsName;
+  final designDetailsImage;
+  final designDetailsPrice;
+  final designDetailsDescription;
 
-DesignDetails({
-  this.designDetailsName,
-  this.designDetailsImage,
-  this.designDetailsPrice,
-  this.designDetailsDescription,
-})
+  DesignDetails({
+    this.designDetailsName,
+    this.designDetailsImage,
+    this.designDetailsPrice,
+    this.designDetailsDescription,
+  });
 
-  createState(){
+  createState() {
     return DesignDetailsState();
   }
 }
 
-class DesignDetailsState extends State<DesignDetails>{
-  Widget build(context){
+class DesignDetailsState extends State<DesignDetails> {
+  Widget build(context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-      body: Container(
-        height: 300,
-          child:
-            Image.asset(
-                'assets/pallazo.jpg',
-              ),
-           
-       ) ),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: ListView(children: [
+          Container(
+            height: 300,
+            child: GridTile(
+              child: Container(
+                  child: Image.asset(
+                widget.designDetailsImage,
+              )),
+              footer: Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Text(
+                      widget.designDetailsName,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 30.0),
+                    ),
+                    trailing: Text('Price:  \$${widget.designDetailsPrice}',
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purpleAccent)),
+                  )),
+            ),
+          )
+        ])));
   }
 }
