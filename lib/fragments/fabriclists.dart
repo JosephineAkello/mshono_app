304f0a5bf7ef509c38a9e547d2e78fd5d06a5daf
mj <onyangomaureen95@gmail.com>
2019-04-09 23:04:05 +0300

    place list of fabric

--- lib/fragments/fabriclists.dart
+++ lib/fragments/fabriclists.dart
@@ -8,6 +8,154 @@ class FabricLists extends StatefulWidget{
 
  class FabricListsState extends State<FabricLists>{
    Widget build(context){
-     return Container();
+     return Container(
+       child:  GridView.count(crossAxisCount: 2, children: <Widget>[
+          Card(
+              child: GridTile(
+            child: Image.asset(
+              'assets/cotton.jpg',
+            ),
+            footer: Container(
+                color: Colors.white,
+                child: ListTile(
+                    leading: Text(
+                      'Cotton Fabric',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    ))),
+          )),
+          Card(
+              child: GridTile(
+            child: Image.asset(
+              'assets/denim.jpg',
+            ),
+            footer: Container(
+                color: Colors.white,
+                child: ListTile(
+                    leading: Text(
+                      'Denim Fabric',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.favorite_border,
+                      color: Colors.red,
+                    ))),
+          )),
+          Card(
+              child: GridTile(
+            child: Image.asset(
+              'assets/kitenge.jpg',
+            ),
+            footer: Container(
+                color: Colors.white,
+                child: ListTile(
+                    leading: Text(
+                      'Kitenge Fabric',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    ))),
+          )),
+          Card(
+              child: GridTile(
+            child: Image.asset(
+              'assets/lace.jpg',
+            ),
+            footer: Container(
+                color: Colors.white,
+                child: ListTile(
+                    leading: Text(
+                      'Lace Fabric',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    ))),
+          )),
+          Card(
+              child: GridTile(
+            child: Image.asset(
+              'assets/silks.jpg',
+            ),
+            footer: Container(
+                color: Colors.white,
+                child: ListTile(
+                    leading: Text(
+                      'Silk Fabric',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.favorite_border,
+                      color: Colors.red,
+                    ))),
+          )),
+          Card(
+              child: GridTile(
+            child: Image.asset(
+              'assets/velvet.jpg',
+            ),
+            footer: Container(
+                color: Colors.white,
+                child: ListTile(
+                    leading: Text(
+                      'Velvet Fabric',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.favorite_border,
+                      color: Colors.red,
+                    ))),
+          )),
+          Card(
+              child: GridTile(
+            child: Image.asset(
+              'assets/vintage.jpg',
+            ),
+            footer: Container(
+                color: Colors.white,
+                child: ListTile(
+                    leading: Text(
+                      'Vintage Fabric',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.favorite_border,
+                      color: Colors.red,
+                    ))),
+          )),
+          Card(
+              child: GridTile(
+            child: Image.asset(
+              'assets/leathers.jpg',
+            ),
+            footer: Container(
+                color: Colors.white,
+                child: ListTile(
+                  leading: Text(
+                    'Leather Fabric',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  trailing: Icon(
+                    Icons.favorite_border,
+                    color: Colors.red,
+                  ),
+                )),
+          )),
+        ]),
+     );
    }
  }
