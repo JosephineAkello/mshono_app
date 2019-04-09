deb71ec624581df396c47b79ae87db5331599ce5
mj <onyangomaureen95@gmail.com>
2019-04-09 22:59:51 +0300

    createfabric list class

---
+++ lib/fragments/fabriclists.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FabricLists extends StatefulWidget{
+  createState(){
+    return FabricListsState();
+  }
+}
+
+ class FabricListsState extends State<FabricLists>{
+   Widget build(context){
+     return Container();
+   }
+ }
