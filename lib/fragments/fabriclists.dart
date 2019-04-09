e8672170f197e6fb1ad58b8849f3833f467a4246
mj <onyangomaureen95@gmail.com>
2019-04-09 23:21:04 +0300

    place map of fabric items

--- lib/fragments/fabriclists.dart
+++ lib/fragments/fabriclists.dart
@@ -7,6 +7,58 @@ class FabricLists extends StatefulWidget{
 }
 
  class FabricListsState extends State<FabricLists>{
+  var fabricLists = [
+    {
+    "name": "Cotton Fabric",
+      "image": "assets/pallazo.jpg",
+      "description": "buggy pants for women",
+      "price": 200,
+  },
+  {
+    "name": "Denim Fabric",
+      "image": "'assets/denim.jpg'",
+      "description": "buggy pants for women",
+      "price": 200,
+  },
+  {
+    "name": "Kitenge Fabric",
+      "image": "assets/kitenge.jpg",
+      "description": "buggy pants for women",
+      "price": 200,
+  },
+  {
+    "name": "Lace Fabric",
+      "image": "assets/lace.jpg",
+      "description": "buggy pants for women",
+      "price": 200,
+  },
+  {
+    "name": "Silk Fabric",
+      "image": "assets/silks.jpg",
+      "description": "buggy pants for women",
+      "price": 200,
+  },
+  {
+    "name": "Velvet Fabric",
+      "image": "assets/velvet.jpg",
+      "description": "buggy pants for women",
+      "price": 200,
+  },
+  {
+    "name": "Vintage Fabric",
+      "image": "assets/vintage.jpg",
+      "description": "buggy pants for women",
+      "price": 200,
+  },
+  {
+    "name": "Leather Fabric",
+      "image": "assets/leathers.jpg",
+      "description": "buggy pants for women",
+      "price": 200,
+  },
+  ];
+
+
    Widget build(context){
      return Container(
        child:  GridView.count(crossAxisCount: 2, children: <Widget>[
