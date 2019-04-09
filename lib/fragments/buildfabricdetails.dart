e24f5c3bb0ae21ff8bc58856fa12e81fb69ffaf3
mj <onyangomaureen95@gmail.com>
2019-04-09 23:57:03 +0300

    create fabric details

---
+++ lib/fragments/buildfabricdetails.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FabricDetails extends StatefulWidget{
+  createState(){
+    return FabricDetailsState();
+  }
+}
+
+class FabricDetailsState extends State<FabricDetails>{
+  Widget build(context){
+    return Container();
+  }
+}
