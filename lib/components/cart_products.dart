d8964184c5fd83f0d8c3490f7ba99a21723ed51c
mj <onyangomaureen95@gmail.com>
2019-04-10 22:12:38 +0300

    create class constructor

--- lib/components/cart_products.dart
+++ lib/components/cart_products.dart
@@ -7,7 +7,46 @@ class CartProducts  extends StatefulWidget{
 }
 
 class CartProductsState extends State<CartProducts>{
+  var product_on_the_cart = [{
+
+      "name": "Pallazo Pants",
+      "image": "assets/pallazo.jpg",
+      "price": 200,
+      "size": "M",
+      "color": "red",
+      "quantity": 1,
+      
+
+  }];
+
   Widget build(context){
-    return Scaffold();
+    return ListView.builder(
+      itemCount: 2,
+      itemBuilder: (context, index){
+
+      },
+
+    );
+  }
+}
+
+class Single_cart_product extends StatelessWidget{
+  final cart_prod_name;
+  final cart_prod_image;
+  final cart_prod_price;
+  final cart_prod_size;
+  final cart_prod_color;
+  final cart_prod_quantity;
+
+Single_cart_product({
+  this.cart_prod_name,
+  this.cart_prod_image,
+  this.cart_prod_price,
+  this.cart_prod_size,
+  this.cart_prod_color,
+  this.cart_prod_quantity,
+});
+ Widget build(context){
+    return Container();
   }
 }
