4504fe55b07df968994a6aa7220861b3450a3c51
mj <onyangomaureen95@gmail.com>
2019-04-10 20:37:59 +0300

    create cart products class

---
+++ lib/components/cart_products.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CartProducts  extends StatefulWidget{
+   createState(){
+  return CartProductsState();
+}
+}
+
+class CartProductsState extends State<CartProducts>{
+  Widget build(context){
+    return Scaffold();
+  }
+}
