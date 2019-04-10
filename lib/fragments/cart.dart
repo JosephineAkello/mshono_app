f8d43b93f519002b9f15d5c69eca4ef580b4bec0
mj <onyangomaureen95@gmail.com>
2019-04-10 19:24:38 +0300

    add checkout and total amount texts

--- lib/fragments/cart.dart
+++ lib/fragments/cart.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
-class Cart extends StatefulWidget{
-  createState(){
+class Cart extends StatefulWidget {
+  createState() {
     return CartState();
   }
 }
 
-class CartState extends State<Cart>{
-  Widget build(context){
-    return Container();
+class CartState extends State<Cart> {
+  Widget build(context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primaryColor: Colors.pink),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Cart'),
+          ),
+          bottomNavigationBar: Container(
+            child: Row(
+             children: <Widget>[
+               Expanded(
+                 child: ListTile(
+                  title: Text('TOTAL',style: TextStyle(
+                fontWeight: FontWeight.bold,)),
+                  subtitle: Text('\$200'),
+               )),
+               Expanded(
+                 child: MaterialButton(
+                   color: Colors.purple,
+                    shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+                   onPressed: (){},
+                  child: Text('Checkout',
+                  style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20.0),
+                
+               )))
+             ],
+            ),
+          ),
+        ));
   }
 }
