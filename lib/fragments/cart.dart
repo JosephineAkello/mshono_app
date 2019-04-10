824ecd1a2c05f2fa1acde4039310c431dc204939
mj <onyangomaureen95@gmail.com>
2019-04-10 18:18:11 +0300

    create shopping cart class

---
+++ lib/fragments/cart.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Cart extends StatefulWidget{
+  createState(){
+    return CartState();
+  }
+}
+
+class CartState extends State<Cart>{
+  Widget build(context){
+    return Container();
+  }
+}
