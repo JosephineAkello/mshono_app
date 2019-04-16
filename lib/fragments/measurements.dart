d8b564a864fc5c0557a6accdc594217ea70225c5
mj <onyangomaureen95@gmail.com>
2019-04-16 11:55:09 +0300

    place youtube video url and properties

--- lib/fragments/measurements.dart
+++ lib/fragments/measurements.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player/youtube_player.dart';
 
 class Measurements extends StatelessWidget{
+VideoPlayerController _controller;
+
+@override
   Widget build(context){
     return Scaffold(
         appBar: AppBar(title: Text('Measurements,'),
@@ -14,7 +18,17 @@ class Measurements extends StatelessWidget{
       body: ListView(
        children: <Widget>[
          Card(
-           child: ListTile(),
+           child:  ListTile(
+             title:YoutubePlayer(
+             context: context,
+             source: "https://www.youtube.com/watch?v=ilg0oMZ455g",
+             quality: YoutubeQuality.MEDIUM,
+             callbackController: (controller){
+               _controller = controller;
+             },
+           ),
+           subtitle: Text('Watch this video for guides on how to take measurements'),
+           ),
          ),
          Card(
            child: ListTile(),
