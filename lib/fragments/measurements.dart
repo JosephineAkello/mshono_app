9a96d53b0c0fab7df77370f794f017e62a90f641
mj <onyangomaureen95@gmail.com>
2019-04-16 11:43:28 +0300

    create a listview of items

--- lib/fragments/measurements.dart
+++ lib/fragments/measurements.dart
@@ -2,9 +2,44 @@ import 'package:flutter/material.dart';
 
 class Measurements extends StatelessWidget{
   Widget build(context){
-    return MaterialApp(
-      home: Scaffold(
-        
+    return Scaffold(
+        appBar: AppBar(title: Text('Measurements,'),
+        ),
+        floatingActionButton: FloatingActionButton(
+           backgroundColor: Colors.purple[100],
+          shape: CircleBorder(),
+          child: Icon(Icons.note_add),
+          onPressed: (){},
+        ),
+      body: ListView(
+       children: <Widget>[
+         Card(
+           child: ListTile(),
+         ),
+         Card(
+           child: ListTile(),
+         ),
+         Card(
+           child: ListTile(),
+         ),
+         Card(
+           child: ListTile(),
+         ),
+         Card(
+           child: ListTile(),
+         ),
+         Card(
+           child: ListTile(),
+         ),
+         Card(
+           child: ListTile(),
+         ),
+         Card(
+           child: ListTile(),
+         ),Card(
+           child: ListTile(),
+         ),
+       ],
       ),
     );
   }
