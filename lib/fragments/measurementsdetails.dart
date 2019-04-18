d716e0da80e2abb734316b11387a467616199cda
mj <onyangomaureen95@gmail.com>
2019-04-18 17:25:18 +0300

    place textform fields

--- lib/fragments/measurementsdetails.dart
+++ lib/fragments/measurementsdetails.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
 
-class MeasurementDetails extends StatefulWidget{
-  createState(){
+class MeasurementDetails extends StatefulWidget {
+  createState() {
     return MeasurementDetailsState();
   }
 }
 
-class MeasurementDetailsState extends State<MeasurementDetails>{
-  Widget build(context){
+class MeasurementDetailsState extends State<MeasurementDetails> {
+  Widget build(context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Measurements Details', style: TextStyle(color: Colors.pink, fontSize: 25, fontWeight: FontWeight.bold),),
-      centerTitle: true,
+        title: Text(
+          'Measurements Details',
+          style: TextStyle(
+              color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Form(
+          child: Column(children: [
+            TextFormField(
+              decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: 'Please enter your name',
+                  labelText: 'Name'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: 'Please enter your design choice',
+                  labelText: 'Design Choice'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: 'Please enter your gender',
+                  labelText: 'Male / Female'),
+            ),
+          ]),
+        ),
       ),
     );
   }
